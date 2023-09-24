ba9a15486e9b49d5334f149aa5ba3890a90adc26
alfrddjq <145941632+alfrddjq@users.noreply.github.com>
2023-09-24 20:54:11 -0300

    CSS y JS

---
+++ javascript-proyecto-final.js
@@ -0,0 +1,55 @@
+function mostrargral(){
+    document.getElementById('info').style.display= 'block'
+}
+
+function ocultargral(){
+    document.getElementById('info').style.display= 'none'
+}
+
+function mostrarform(){
+    document.getElementById('form').style.display= 'block'
+}
+
+function ocultarform(){
+    document.getElementById('form').style.display= 'none'
+}
+
+function mostrarexperiencia(){
+    document.getElementById('experiencia').style.display= 'block'
+}
+
+function ocultarexperiencia(){
+    document.getElementById('experiencia').style.display= 'none'
+}
+
+function mostraraptitudes(){
+    document.getElementById('apt').style.display= 'block'
+}
+
+function ocultaraptitudes(){
+    document.getElementById('apt').style.display= 'none'
+}
+
+function mostrarsoft(){
+    document.getElementById('soft').style.display= 'block'
+}
+
+function ocultarsoft(){
+    document.getElementById('soft').style.display= 'none'
+}
+
+function mostraridiom(){
+    document.getElementById('idiom').style.display= 'block'
+}
+
+function ocultaridiom(){
+    document.getElementById('idiom').style.display= 'none'
+}
+
+function mostrarcont(){
+    document.getElementById('cont').style.display= 'block'
+}
+
+function ocultarcont(){
+    document.getElementById('cont').style.display= 'none'
+}
